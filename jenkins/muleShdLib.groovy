a7010878878e0b5b1a95bd3dfd1fa09ef435fc88
rpaulnu <73297165+rpaulnu@users.noreply.github.com>
2021-02-25 17:46:40 +0100

    Update muleShdLib.groovy

--- jenkins/muleShdLib.groovy
+++ jenkins/muleShdLib.groovy
@@ -165,8 +165,8 @@ def runMulesoftPipeline(apiName) {
 		container('mule-builder') {
 			script {
 				try {
-		      uploadAssetToExchange("")
-                      deploy("")
+		      				uploadAssetToExchange("")
+                      				deploy("")
 				} catch(Exception e) {
 					println "There has been an error deploying mulesoft API"
 					throw e
