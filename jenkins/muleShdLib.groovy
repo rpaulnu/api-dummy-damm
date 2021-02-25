2159c90303c663977029fc611e6d3073a30d6a12
Rodrigo Nuñez Paul <rnunepau@everis.com>
2021-02-25 16:16:26 +0100

    omit upload to exchange

--- jenkins/muleShdLib.groovy
+++ jenkins/muleShdLib.groovy
@@ -165,7 +165,7 @@ def runMulesoftPipeline(apiName) {
 		container('mule-builder') {
 			script {
 				try {
-                    deploy("")
+                     deploy("")
 				} catch(Exception e) {
 					println "There has been an error deploying mulesoft API"
 					throw e
