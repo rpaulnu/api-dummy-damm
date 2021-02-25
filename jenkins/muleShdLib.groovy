eab2b205f2b0571ab2bb3d3f92828217a4d7c7c9
Rodrigo Nuñez Paul <rnunepau@everis.com>
2021-02-25 12:00:29 +0100

    change json slurper classic

--- jenkins/muleShdLib.groovy
+++ jenkins/muleShdLib.groovy
@@ -101,8 +101,8 @@ def retrieveMulesoftVariables() {
     def slurper = new JsonSlurper()
     echo response
 
-    //def ACCESS_TOKEN = slurper.parseText(response).access_token
-    //echo ACCESS_TOKEN
+    def ACCESS_TOKEN = slurper.parseText(response).access_token
+    echo ACCESS_TOKEN
     /*url = "curl -s -X GET https://${ANYPOINT_PLATFORM_URL}/accounts/api/me -H \"Authorization:Bearer ${ACCESS_TOKEN}\""
     response = slurper.parseText(url.execute().text)
 
